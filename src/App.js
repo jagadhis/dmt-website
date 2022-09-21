8c99cadd306a532bde919a5cb17100ea9528ccac
jagadhis <jwaran78@gmail.com>
2022-09-21 09:26:30 +0530

    Changes Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { useState, useEffect } from "react";
-import Maintanence from './Pages/Maintanence';
 import { Navigation } from "./Components/navigation";
 import JsonData from "./Data/data.json";
 import { Header } from "./Components/header";
