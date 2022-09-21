5b38b1bdb6559f3cbed7771a0ddb837343260350
jagadhis <jwaran78@gmail.com>
2022-09-21 09:23:44 +0530

    Called Function And Added Data Folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { useState, useEffect } from "react";
 import Maintanence from './Pages/Maintanence';
+import { Navigation } from "./components/navigation";
+import JsonData from "./data/data.json";
+import { Header } from "./components/header";
 
 function App() {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
   return (
     <div className="App">
+       <Navigation />
+      <Header data={landingPageData.Header} />
       <Maintanence />
+
     </div>
   );
 }
