2aff20a161ce61f4dc95f7be724667c5c45fb250
jagadhis <jwaran78@gmail.com>
2022-09-28 19:28:21 +0530

    Added Desgins And Pages

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { Navigation } from "./Components/navigation";
+import { Header } from "./Components/header";
+import { Features } from "./Components/features";
+import { About } from "./Components/about";
+import { Services } from "./Components/services";
+import { Gallery } from "./Components/gallery";
+import { Testimonials } from "./Components/testimonials";
+import { Team } from "./Components/Team";
+import { Contact } from "./Components/contact";
+import JsonData from "./Data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery data={landingPageData.Gallery}/>
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </div>
+  );
+};
+
+export default App;
