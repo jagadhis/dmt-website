8c29b64cda5e5f038ded17c9481ccb477dff708b
jagadhis <jwaran78@gmail.com>
2022-09-28 19:33:35 +0530

    CHANGES

--- src/Components/navigation.jsx
+++ src/Components/navigation.jsx
@@ -16,8 +16,8 @@ export const Navigation = (props) => {
               <span className='icon-bar'></span>{' '}
             </button>
             <a className='navbar-brand page-scroll' href='#page-top'>
-              React Landing Page
-            </a>{' '}
+              DMT
+            </a>
           </div>
   
           <div
